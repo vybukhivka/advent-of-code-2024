f0396ac22fcce3744560f187738f13b6a9762f71
vybukhivka <viktorbakhovka@gmail.com>
2024-12-02 01:08:48 +0200

    more readable solution for part 2(and more performant as well

--- src/day1/day1.2.ts
+++ src/day1/day1.2.ts
@@ -1,18 +1,32 @@
 import { createSortedColumns } from "./day1.1";
 import { data } from "./day1.1-input";
 
-export const part2 = () => {
+export const part2 = (): number => {
 	const { left, right } = createSortedColumns(data);
+	let similarityScore = 0;
 
-	const similarities = left.reduce((similarityScore, curLeft) => {
-		const amountOfMathesOnRight = right.reduce((matchCount, curRight) => {
-			curLeft === curRight && ++matchCount;
+	for (let i = 0; i < left.length; i++) {
+		let nubmerOfMatches = 0;
+		for (let j = 0; j < right.length; j++) {
+			left[i] === right[j] && nubmerOfMatches++;
+		}
 
-			return matchCount;
-		}, 0);
+		similarityScore += left[i] * nubmerOfMatches;
+	}
 
-		return (similarityScore += amountOfMathesOnRight * curLeft);
-	}, 0);
+	return similarityScore;
 
-	return similarities;
+	// Nested reducers(less performant)
+
+	// const similarities = left.reduce((similarityScore, curLeft) => {
+	// 	const amountOfMathesOnRight = right.reduce((matchCount, curRight) => {
+	// 		curLeft === curRight && ++matchCount;
+
+	// 		return matchCount;
+	// 	}, 0);
+
+	// 	return similarityScore + amountOfMathesOnRight * curLeft;
+	// }, 0);
+
+	// return similarities;
 };
