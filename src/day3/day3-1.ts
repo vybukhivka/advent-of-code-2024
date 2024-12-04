0bcd793cf11a05dd0352da901801c40f59f5e09d
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-12-04 02:06:22 +0200

    day3 part 1

--- src/day3/day3-1.ts
+++ src/day3/day3-1.ts
@@ -1,14 +1,28 @@
 const getInputData = async () => {
-	try {
-		const response = await fetch("/day3-input.txt");
-		const data = await response.text();
-		return data;
-	} catch (error) {
-		console.error("Error fetching file:", error);
-	}
+  try {
+    const response = await fetch("/day3-input.txt");
+    const data = await response.text();
+    return data;
+  } catch (error) {
+    console.error("Error fetching file:", error);
+  }
 };
 
-export async function day3part1() {
-	const data = await getInputData();
-	console.log(data);
+const example =
+  "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+const data = await getInputData();
+
+export function day3part1() {
+  const mulReg = /mul\(\d{1,3}\,\d{1,3}\)/g;
+  const matchedMuls = data.match(mulReg);
+  let mulSum = 0;
+
+  for (const mul of matchedMuls) {
+    const [digits] = mul.match(/\d*\,\d*/).flat();
+    const leftDigit = digits.match(/\d+/)[0];
+    const rightDigit = digits.match(/,(\d+)/)[1];
+
+    mulSum += leftDigit * rightDigit;
+  }
+  console.log(mulSum);
 }
