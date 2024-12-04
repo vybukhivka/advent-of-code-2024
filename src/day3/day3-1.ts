1ab62b2cdb05c45ae085fd09a125af889bcad145
vybukhivka <viktorbakhovka@gmail.com>
2024-12-04 22:29:10 +0200

    day 3 part 2

--- src/day3/day3-1.ts
+++ src/day3/day3-1.ts
@@ -1,42 +1,42 @@
 export const getInputData = async () => {
-  try {
-    const response = await fetch("/day3-input.txt");
-    const data = await response.text();
-    return data;
-  } catch (error) {
-    console.error("Error fetching file:", error);
-  }
+	try {
+		const response = await fetch("/day3-input.txt");
+		const data = await response.text();
+		return data;
+	} catch (error) {
+		console.error("Error fetching file:", error);
+	}
 };
 
 export const example =
-  "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+	"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 export const data = await getInputData();
 
 export function findMuls(inputData: string) {
-  const mulReg = /mul\(\d{1,3}\,\d{1,3}\)/g;
-  return inputData.match(mulReg);
+	const mulReg = /mul\(\d{1,3}\,\d{1,3}\)/g!;
+	const muls = inputData.match(mulReg) ?? [];
+	return muls;
 }
 
 export function calcMulsWithSum(muls: string[]) {
-  let mulSum = 0;
-  console.log(muls);
+	let mulSum = 0;
 
-  for (const mul of muls) {
-    const [digits]: RegExpMatchArray = mul.match(/\d*\,\d*/)!;
-    if (!digits) throw new Error("No digits found");
-    const leftDigit = digits.match(/\d+/)![0];
-    const rightDigit = digits.match(/,(\d+)/)![1];
+	for (const mul of muls) {
+		const [digits]: RegExpMatchArray = mul.match(/\d*\,\d*/)!;
+		if (!digits) throw new Error("No digits found");
+		const leftDigit = digits.match(/\d+/)![0];
+		const rightDigit = digits.match(/,(\d+)/)![1];
 
-    mulSum += Number(leftDigit) * Number(rightDigit);
-  }
-  return mulSum;
+		mulSum += Number(leftDigit) * Number(rightDigit);
+	}
+	return mulSum;
 }
 
 export function day3part1() {
-  if (!data) throw new Error("No data found");
-  const matchedMuls = findMuls(data);
-  if (!matchedMuls) throw new Error("No matched muls");
-  const sum = calcMulsWithSum(matchedMuls);
-  console.log(sum);
-  return sum;
+	if (!data) throw new Error("No data found");
+	const matchedMuls = findMuls(data);
+	if (!matchedMuls) throw new Error("No matched muls");
+	const sum = calcMulsWithSum(matchedMuls);
+	// console.log(sum);
+	return sum;
 }
--- src/day3/day3-2.ts
+++ src/day3/day3-2.ts
@@ -1,11 +1,11 @@
 import { calcMulsWithSum, data, findMuls } from "./day3-1";
 
-const data = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
+const exampleData = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
 
-function findDont(inputData) {
+function findDont(inputData: string) {
 	const dontReg = /don\'t\(\)/g;
 	const dontMatches = inputData.matchAll(dontReg);
-	const compareFn = (a, b) => (a > b ? -1 : 0);
+	const compareFn = (a: number, b: number) => (a > b ? -1 : 0);
 	let dontIndexes = [];
 
 	for (const match of dontMatches) {
@@ -15,10 +15,10 @@ function findDont(inputData) {
 	return dontIndexes.sort(compareFn);
 }
 
-function findDo(inputData) {
+function findDo(inputData: string) {
 	const dontReg = /do\(\)/g;
 	const doMatches = inputData.matchAll(dontReg);
-	const compareFn = (a, b) => (a > b ? -1 : 0);
+	const compareFn = (a: number, b: number) => (a > b ? -1 : 0);
 	let doIndexes = [];
 
 	for (const match of doMatches) {
@@ -28,20 +28,14 @@ function findDo(inputData) {
 	return doIndexes.sort(compareFn);
 }
 
-function createMulSelections(doArr, dontArr) {
+function createMulSelections(doArr: number[], dontArr: number[]) {
+	// debugger;
 	const disabled = [];
 	const enabled = [];
 	let currentDont = 0;
 	let currentDo = 0;
 	let lastDont = -1;
 	let lastDo = -1;
-
-	console.log("dont", dontArr);
-	console.log("do  ", doArr);
-
-	// do() enables future mul instructions
-	// from start of the string and till first don't muls must be executed
-	// don't() disable future mul instructions
 
 	// enabled mul! from: 18362 till: 19050
 	for (let i = 0; i < dontArr.length; i++) {
@@ -56,13 +50,6 @@ function createMulSelections(doArr, dontArr) {
 			}
 		}
 
-		if (currentDo === lastDo && lastDont === currentDont) {
-			console.log("returned");
-			return;
-		}
-
-		// then find next dont that > then last do
-		// this selection with enabled mul
 		for (const doIndex of doArr) {
 			if (currentDont < doIndex && doIndex !== lastDo) {
 				currentDo = doIndex;
@@ -76,27 +63,34 @@ function createMulSelections(doArr, dontArr) {
 	return { enabled, disabled };
 }
 
-function multiplySelection(enabledRange, str) {
-	let sum = 0;
+function multiplySelection(enabledRange: number[][], str: string) {
+	let sum: number = 0;
+
 	for (const [start, end] of enabledRange) {
-		console.log(str.slice(start, end));
-		const muls = findMuls(str.slice(start, end));
+		const strEnabled = str.slice(start, end);
+		const muls = findMuls(strEnabled);
+		// console.log("muls", muls);
+
+		// if (muls === null) throw new Error("muls === null");
 		sum += calcMulsWithSum(muls);
 	}
 	return sum;
 }
 
-// Store muls that placed before 'don't()' and after 'do()'
 export function day3part2() {
-	const dataCopy = data;
-	const doIndexes = findDo(data);
-	const dontIndexes = findDont(data);
-	const { enabled, disabled } = createMulSelections(doIndexes, dontIndexes);
+	const dataCopy: string = data;
+	const doIndexes = findDo(dataCopy);
+	const dontIndexes = findDont(dataCopy);
 
-	const sum = multiplySelection(enabled, data);
+	console.log("dont", dontIndexes);
+	console.log("do  ", doIndexes);
 
-	console.log("enabled", enabled);
-	// console.log(disabled);
+	const { enabled } = createMulSelections(doIndexes, dontIndexes);
+	console.log(enabled);
+
+	const sum = multiplySelection(enabled, dataCopy);
+
+	// console.log("enabled", enabled);
 	console.log(sum);
 
 	return true;
