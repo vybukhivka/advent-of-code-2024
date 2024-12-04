82bd14fb8c957e4fd664b3caabfc9590785f3e20
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-12-04 03:11:01 +0200

    wip: storing indexes to create selections

--- src/day3/day3-2.ts
+++ src/day3/day3-2.ts
@@ -1,10 +1,37 @@
-import { data, example, findMuls } from "./day3-1";
+import { data, findMuls } from "./day3-1";
 
+const example = `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`;
+
+function findDont(inputData) {
+  const dontReg = /don\'t\(\)/g;
+  return inputData.match(dontReg);
+}
+
+function findDo(inputData) {
+  const dontReg = /do\(\)/g;
+  return inputData.match(dontReg);
+}
+
+// Store muls that placed before 'don't()' and after 'do()'
 export function day3part2() {
-  const muls = findMuls(example);
-  console.log(data);
+  const dataCopy = data;
+  const muls = findMuls(data);
+  const donts = findDont(data);
+  const dos = findDo(data);
 
-  console.log(muls);
+  const regDont = /don\'t\(\)/g;
+  const dons = data.matchAll(regDont);
+  for (const don of dons) {
+    console.log(don);
+  }
+
+  // console.log(data);
+  // console.log(muls);
+  // console.log(donts);
+  // console.log(dos);
+
+  const dontIndex = dataCopy.indexOf("don't()");
+  console.log(dontIndex);
 
   return true;
 }
