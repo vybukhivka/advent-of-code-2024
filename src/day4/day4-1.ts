a5d0b5e2dbbab663d7dcfe1943ab046dcf78b1b8
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-12-05 02:52:38 +0200

    find all indexes of X

--- src/day4/day4-1.ts
+++ src/day4/day4-1.ts
@@ -1,9 +1,61 @@
 import { getInputData } from "../day3/day3-1";
 
 const data = await getInputData("/day4-input.txt");
+const exampleData = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`;
+
+type AllIndexes = number[][];
+
+// 1. Find all X indexes
+function findIndexes(splitedStrings: string[]): AllIndexes {
+  const xIndexes: AllIndexes = [];
+  for (let i = 0; i < splitedStrings.length; i++) {
+    const matchedIndexes: number[] = [];
+    const regexp = /X/g;
+    const matches = splitedStrings[i].matchAll(regexp);
+
+    for (const match of matches) {
+      matchedIndexes.push(match.index);
+    }
+    xIndexes.push(matchedIndexes);
+  }
+  return xIndexes;
+}
+// 2. Horizontal search in two directions
+function horizontalSearch(splitedStrings: string[]) {
+  for (let i = 0; i < splitedStrings.length; i++) {
+    const indexArr = [];
+    const regexp = /X/g;
+    const matches = splitedStrings[i].matchAll(regexp);
+
+    for (const match of matches) {
+      indexArr.push(match.index);
+    }
+    console.log(indexArr);
+    break;
+  }
+}
+// 3. Vertical search in two directions
+function verticalSearch(puzzleInput) {}
+// 4. Diagonal search in four directions
+function diagonalSearch(puzzleInput) {}
 
 export function day4part1() {
-	console.log(data);
+  const dataCopy = exampleData;
+  const splitedData = dataCopy.split("\n");
 
-	return true;
+  const allIndexes = findIndexes(splitedData);
+
+  console.log(splitedData);
+  console.log(allIndexes);
+
+  return true;
 }
